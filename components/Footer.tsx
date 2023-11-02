fcc1268c4e1f205e2d02db89c3a190229354189f
Simon Achkar <simon@simonachkar.com>
2023-11-02 14:16:57 -0300

    Removed footer image (temp)

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+// import Image from 'next/image'
 import { getDictionary } from '../get-dictionaries'
 
 export default async function Footer({ lang }: any) {
@@ -7,14 +7,14 @@ export default async function Footer({ lang }: any) {
 
     return (
         <footer className="w-full px-5 pb-16 text-xs sm:pb-0 max-w-md">
-            <Image
-                src="/logo.png"
-                alt="Simon Achkar Logo"
-                className="dark:invert pb-2 rotate-0 pl-1"
-                width={43}
-                height={43}
-                priority
-            />
+             {/* <Image
+               src="/logo.png"
+               alt="Simon Achkar Logo"
+             className="dark:invert pb-2 rotate-0 pl-1"
+                 width={43}
+                 height={43}
+                 priority
+             /> */}
             <p className='mt-1.5'>{dictionary['Footer'].copyright}</p>
             <p className='mt-1.5'>{dictionary['Footer'].builtWith}</p>
             <p className='mt-1.5'>{dictionary['Footer'].madeWith} <span className='ml-0.5'>&hearts;</span></p>
